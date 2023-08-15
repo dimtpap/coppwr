58811db96a314b6088e0094745ff42399baf7e3f
Dimitris Papaioannou <dimtpap@protonmail.com>
2023-08-15 17:18:34 +0300

    backend: Move backend interfaces to mod.rs

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -19,4 +19,82 @@ mod pipewire;
 pub mod pods;
 mod util;
 
-pub use self::pipewire::*;
+use std::{collections::BTreeMap, sync::mpsc};
+
+#[cfg(feature = "pw_v0_3_77")]
+use std::sync::OnceLock;
+
+pub use ::pipewire as pw;
+use pw::{permissions::Permissions, types::ObjectType};
+
+use self::pods::profiler::Profiling;
+
+pub enum ObjectMethod {
+    ClientGetPermissions {
+        index: u32,
+        num: u32,
+    },
+    ClientUpdatePermissions(Vec<Permissions>),
+    ClientUpdateProperties(BTreeMap<String, String>),
+    MetadataSetProperty {
+        subject: u32,
+        key: String,
+        type_: Option<String>,
+        value: Option<String>,
+    },
+    MetadataClear,
+}
+
+pub enum Request {
+    Stop,
+    CreateObject(ObjectType, String, Vec<(String, String)>),
+    DestroyObject(u32),
+    LoadModule {
+        module_dir: Option<String>,
+        name: String,
+        args: Option<String>,
+        props: Option<Vec<(String, String)>>,
+    },
+    CallObjectMethod(u32, ObjectMethod),
+}
+
+pub enum Event {
+    GlobalAdded(u32, ObjectType, Option<BTreeMap<String, String>>),
+    GlobalRemoved(u32),
+    GlobalInfo(u32, Box<[(&'static str, String)]>),
+    GlobalProperties(u32, BTreeMap<String, String>),
+    ClientPermissions(u32, u32, Vec<Permissions>),
+    ProfilerProfile(Vec<Profiling>),
+    MetadataProperty {
+        id: u32,
+        subject: u32,
+        key: Option<String>,
+        type_: Option<String>,
+        value: Option<String>,
+    },
+    Stop,
+}
+
+#[cfg(feature = "pw_v0_3_77")]
+static REMOTE_VERSION: OnceLock<(u32, u32, u32)> = OnceLock::new();
+#[cfg(feature = "pw_v0_3_77")]
+pub fn remote_version<'a>() -> Option<&'a (u32, u32, u32)> {
+    REMOTE_VERSION.get()
+}
+
+pub fn run(
+    remote: String,
+) -> (
+    std::thread::JoinHandle<()>,
+    mpsc::Receiver<Event>,
+    pw::channel::Sender<Request>,
+) {
+    let (sx, rx) = mpsc::channel::<Event>();
+    let (pwsx, pwrx) = pw::channel::channel::<Request>();
+
+    (
+        std::thread::spawn(move || self::pipewire::pipewire_thread(remote.as_str(), sx, pwrx)),
+        rx,
+        pwsx,
+    )
+}
--- src/backend/pipewire.rs
+++ src/backend/pipewire.rs
@@ -14,96 +14,17 @@
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::{
-    cell::RefCell,
-    collections::{BTreeMap, HashMap},
-    rc::Rc,
-    sync::mpsc,
-};
+use std::{cell::RefCell, collections::HashMap, rc::Rc, sync::mpsc};
 
-#[cfg(feature = "pw_v0_3_77")]
-use std::sync::OnceLock;
+use pw::proxy::ProxyT;
 
-use pipewire as pw;
-use pw::{permissions::Permissions, proxy::ProxyT, types::ObjectType};
+use super::{bind::*, *};
 
-use super::{
-    bind::{BindError, BoundGlobal},
-    pods::profiler::Profiling,
-    util,
-};
-
-pub enum ObjectMethod {
-    ClientGetPermissions {
-        index: u32,
-        num: u32,
-    },
-    ClientUpdatePermissions(Vec<Permissions>),
-    ClientUpdateProperties(BTreeMap<String, String>),
-    MetadataSetProperty {
-        subject: u32,
-        key: String,
-        type_: Option<String>,
-        value: Option<String>,
-    },
-    MetadataClear,
-}
-
-pub enum Request {
-    Stop,
-    CreateObject(ObjectType, String, Vec<(String, String)>),
-    DestroyObject(u32),
-    LoadModule {
-        module_dir: Option<String>,
-        name: String,
-        args: Option<String>,
-        props: Option<Vec<(String, String)>>,
-    },
-    CallObjectMethod(u32, ObjectMethod),
-}
-
-pub enum Event {
-    GlobalAdded(u32, ObjectType, Option<BTreeMap<String, String>>),
-    GlobalRemoved(u32),
-    GlobalInfo(u32, Box<[(&'static str, String)]>),
-    GlobalProperties(u32, BTreeMap<String, String>),
-    ClientPermissions(u32, u32, Vec<Permissions>),
-    ProfilerProfile(Vec<Profiling>),
-    MetadataProperty {
-        id: u32,
-        subject: u32,
-        key: Option<String>,
-        type_: Option<String>,
-        value: Option<String>,
-    },
-    Stop,
-}
-
-#[cfg(feature = "pw_v0_3_77")]
-static REMOTE_VERSION: OnceLock<(u32, u32, u32)> = OnceLock::new();
-#[cfg(feature = "pw_v0_3_77")]
-pub fn remote_version<'a>() -> Option<&'a (u32, u32, u32)> {
-    REMOTE_VERSION.get()
-}
-
-pub fn run(
-    remote: String,
-) -> (
-    std::thread::JoinHandle<()>,
-    mpsc::Receiver<Event>,
-    pw::channel::Sender<Request>,
+pub fn pipewire_thread(
+    remote: &str,
+    sx: mpsc::Sender<Event>,
+    pwrx: pw::channel::Receiver<Request>,
 ) {
-    let (sx, rx) = mpsc::channel::<Event>();
-    let (pwsx, pwrx) = pw::channel::channel::<Request>();
-
-    (
-        std::thread::spawn(move || pipewire_thread(remote.as_str(), sx, pwrx)),
-        rx,
-        pwsx,
-    )
-}
-
-fn pipewire_thread(remote: &str, sx: mpsc::Sender<Event>, pwrx: pw::channel::Receiver<Request>) {
     // Proxies created by core.create_object
     struct LocalProxy(pw::proxy::Proxy, pw::proxy::ProxyListener);
 
