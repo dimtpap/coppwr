14b80b51ca6f82d97d2a080e54494ca3580934fc
Dimitris Papaioannou <dimtpap@protonmail.com>
2023-08-25 00:11:31 +0300

    ui/profiler: Only realloc on more max profilings
    Arguement passed to reverse should be calculated based on length and not capacity. Previously it was allocating for an amount slightly lower than the max profilings if the queue wasn't full. Also do not shrink as it causes lots of reallocations if the DragValue is dragged

--- src/ui/profiler.rs
+++ src/ui/profiler.rs
@@ -50,13 +50,9 @@ impl Profiler {
             return;
         }
 
-        // Adjust driver queues first
         for profilings in self.drivers.values_mut() {
             if profilings.capacity() < self.max_profilings {
-                profilings.reserve(self.max_profilings - profilings.capacity());
-            } else if profilings.len() > self.max_profilings {
-                profilings.drain(0..(profilings.len() - self.max_profilings));
-                profilings.shrink_to(self.max_profilings);
+                profilings.reserve(self.max_profilings - profilings.len());
             }
         }
 
