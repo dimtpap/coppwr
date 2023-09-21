1c80e0e0d535ae2b4182ed740d187a5c59bfe31c
Dimitris Papaioannou <dimtpap@protonmail.com>
2023-09-21 13:22:49 +0300

    ui: Format literal

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -446,7 +446,9 @@ impl eframe::App for App {
 
                             ui.separator();
 
-                            ui.label("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.");
+                            ui.label("This program is free software: you can redistribute it and/or modify it \
+                                            under the terms of the GNU General Public License version 3 as published \
+                                            by the Free Software Foundation.");
                         });
                     });
 
