6dab3cd1d7db09d4132e9f4ac207fc1bc1845f98
dimtpap <dimtpap@protonmail.com>
2024-10-09 20:53:29 +0300

    Update ashpd
    Move non-portal and portal implementations of Connection to their own
    modules to clean up #cfgs

--- src/backend/connection.rs
+++ src/backend/connection.rs
@@ -14,12 +14,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
-#[cfg(feature = "xdg_desktop_portals")]
-use ashpd::desktop::Session;
-
 use pipewire as pw;
-
-use super::{util, RemoteInfo};
 
 #[derive(Debug)]
 pub enum Error {
@@ -60,123 +55,157 @@ impl std::fmt::Display for Error {
 
 impl std::error::Error for Error {}
 
+#[cfg(not(feature = "xdg_desktop_portals"))]
+mod connection {
+    use pipewire as pw;
+
+    use crate::backend::{util, RemoteInfo};
+
+    use super::Error;
+
+    pub struct Connection(pw::core::Core);
+
+    impl Connection {
+        pub fn connect(
+            context: &pw::context::Context,
+            context_properties: Vec<(String, String)>,
+            remote: RemoteInfo,
+        ) -> Result<Self, Error> {
+            let RemoteInfo::Regular(remote) = remote;
+            Ok(Self(util::connect_override_env(
+                context,
+                util::key_val_to_props(context_properties.into_iter()),
+                remote,
+            )?))
+        }
+
+        pub const fn core(&self) -> &pw::core::Core {
+            &self.0
+        }
+    }
+}
+
 #[cfg(feature = "xdg_desktop_portals")]
-mod portals {
+mod connection {
     use std::os::fd::OwnedFd;
 
     use ashpd::{
-        desktop::{screencast::SourceType, Session},
+        desktop::{
+            screencast::{Screencast, SourceType},
+            Session,
+        },
         enumflags2::BitFlags,
     };
+    use pipewire as pw;
 
-    pub fn open_screencast_remote<'s>(
-        types: BitFlags<SourceType>,
-        multiple: bool,
-    ) -> Result<(OwnedFd, Session<'s>), ashpd::Error> {
-        pollster::block_on(async {
-            use ashpd::desktop::screencast::{CursorMode, PersistMode, Screencast};
+    use crate::backend::{util, RemoteInfo};
 
-            let proxy = Screencast::new().await?;
-            let session = proxy.create_session().await?;
+    use super::Error;
 
-            proxy
-                .select_sources(
-                    &session,
-                    CursorMode::Hidden,
-                    types,
-                    multiple,
-                    None,
-                    PersistMode::DoNot,
-                )
-                .await?;
-
-            proxy
-                .start(&session, &ashpd::WindowIdentifier::default())
-                .await?;
-
-            let fd = proxy.open_pipe_wire_remote(&session).await?;
-
-            Ok((fd, session))
-        })
+    enum PortalSession<'a, 'b> {
+        Screencast(Session<'a, Screencast<'b>>),
     }
 
-    pub fn open_camera_remote() -> Result<Option<OwnedFd>, ashpd::Error> {
-        pollster::block_on(ashpd::desktop::camera::request())
-    }
-}
-
-#[cfg(not(feature = "xdg_desktop_portals"))]
-pub struct Connection(pw::core::Core);
-#[cfg(not(feature = "xdg_desktop_portals"))]
-impl Connection {
-    pub fn connect(
-        context: &pw::context::Context,
-        context_properties: Vec<(String, String)>,
-        remote: RemoteInfo,
-    ) -> Result<Self, Error> {
-        let RemoteInfo::Regular(remote) = remote;
-        Ok(Self(util::connect_override_env(
-            context,
-            util::key_val_to_props(context_properties.into_iter()),
-            remote,
-        )?))
-    }
-
-    pub const fn core(&self) -> &pw::core::Core {
-        &self.0
-    }
-}
-
-#[cfg(feature = "xdg_desktop_portals")]
-pub enum Connection<'s> {
-    Simple(pw::core::Core),
-    PortalWithSession(pw::core::Core, Session<'s>),
-}
-
-#[cfg(feature = "xdg_desktop_portals")]
-impl<'s> Connection<'s> {
-    pub fn connect(
-        context: &pw::context::Context,
-        context_properties: Vec<(String, String)>,
-        remote: RemoteInfo,
-    ) -> Result<Self, Error> {
-        let context_properties = util::key_val_to_props(context_properties.into_iter());
-
-        match remote {
-            RemoteInfo::Regular(remote_name) => Ok(Self::Simple(util::connect_override_env(
-                context,
-                context_properties,
-                remote_name,
-            )?)),
-            RemoteInfo::Screencast { types, multiple } => {
-                let (fd, session) = portals::open_screencast_remote(types, multiple)?;
-
-                Ok(Self::PortalWithSession(
-                    context.connect_fd(fd, Some(context_properties))?,
-                    session,
-                ))
-            }
-            RemoteInfo::Camera => Ok(Self::Simple(context.connect_fd(
-                portals::open_camera_remote()?.ok_or(Error::PortalUnavailable)?,
-                Some(context_properties),
-            )?)),
-        }
-    }
-
-    pub const fn core(&self) -> &pw::core::Core {
-        match self {
-            Self::Simple(core) | Self::PortalWithSession(core, _) => core,
-        }
-    }
-}
-
-#[cfg(feature = "xdg_desktop_portals")]
-impl<'s> Drop for Connection<'s> {
-    fn drop(&mut self) {
-        if let Self::PortalWithSession(_, session) = self {
-            if let Err(e) = pollster::block_on(session.close()) {
-                eprintln!("Error when stopping portal session: {e}");
+    impl PortalSession<'_, '_> {
+        fn close(&self) -> Result<(), ashpd::Error> {
+            match self {
+                Self::Screencast(s) => pollster::block_on(s.close()),
             }
         }
     }
+
+    impl<'a, 'b> From<Session<'a, Screencast<'b>>> for PortalSession<'a, 'b> {
+        fn from(value: Session<'a, Screencast<'b>>) -> Self {
+            Self::Screencast(value)
+        }
+    }
+
+    pub struct Connection<'a, 'b> {
+        core: pw::core::Core,
+        session: Option<PortalSession<'a, 'b>>,
+    }
+
+    impl<'a, 'b> Connection<'a, 'b> {
+        pub fn connect(
+            context: &pw::context::Context,
+            context_properties: Vec<(String, String)>,
+            remote: RemoteInfo,
+        ) -> Result<Self, Error> {
+            fn open_screencast_remote<'a, 'b>(
+                types: BitFlags<SourceType>,
+                multiple: bool,
+            ) -> Result<(OwnedFd, Session<'a, Screencast<'b>>), ashpd::Error> {
+                pollster::block_on(async {
+                    use ashpd::desktop::{
+                        screencast::{CursorMode, Screencast},
+                        PersistMode,
+                    };
+
+                    let proxy = Screencast::new().await?;
+                    let session = proxy.create_session().await?;
+
+                    proxy
+                        .select_sources(
+                            &session,
+                            CursorMode::Hidden,
+                            types,
+                            multiple,
+                            None,
+                            PersistMode::DoNot,
+                        )
+                        .await?;
+
+                    proxy
+                        .start(&session, &ashpd::WindowIdentifier::default())
+                        .await?;
+
+                    let fd = proxy.open_pipe_wire_remote(&session).await?;
+
+                    Ok((fd, session))
+                })
+            }
+
+            fn open_camera_remote() -> Result<Option<OwnedFd>, ashpd::Error> {
+                pollster::block_on(ashpd::desktop::camera::request())
+            }
+
+            let context_properties = util::key_val_to_props(context_properties.into_iter());
+
+            match remote {
+                RemoteInfo::Regular(remote_name) => Ok(Self {
+                    core: util::connect_override_env(context, context_properties, remote_name)?,
+                    session: None,
+                }),
+                RemoteInfo::Screencast { types, multiple } => {
+                    let (fd, session) = open_screencast_remote(types, multiple)?;
+
+                    Ok(Self {
+                        core: context.connect_fd(fd, Some(context_properties))?,
+                        session: Some(session.into()),
+                    })
+                }
+                RemoteInfo::Camera => Ok(Self {
+                    core: context.connect_fd(
+                        open_camera_remote()?.ok_or(Error::PortalUnavailable)?,
+                        Some(context_properties),
+                    )?,
+                    session: None,
+                }),
+            }
+        }
+
+        pub const fn core(&self) -> &pw::core::Core {
+            &self.core
+        }
+    }
+
+    impl Drop for Connection<'_, '_> {
+        fn drop(&mut self) {
+            if let Some(Err(e)) = self.session.as_ref().map(PortalSession::close) {
+                eprintln!("Error when closing portal session: {e}");
+            }
+        }
+    }
 }
+
+pub use connection::Connection;
